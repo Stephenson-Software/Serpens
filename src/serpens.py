de4b9004f6acadf718af4270e58f3300e4d703e1
dmccoystephenson <dmccoystephenson@gmail.com>
2022-08-06 19:18:27 -0600

    Added an environment and some methods for drawing it.

--- src/serpens.py
+++ src/serpens.py
@@ -1,6 +1,8 @@
 import pygame
 from config import Config
+from environment import Environment
 from graphik import Graphik
+from snakePart import SnakePart
 
 
 # @author Daniel McCoy Stephenson
@@ -13,12 +15,39 @@ class Serpens:
         self.initializeGameDisplay()
         self.graphik = Graphik(self.gameDisplay)
         self.running = True
+        self.environment = Environment("Serpens", self.config.gridSize)
+        self.initializeLocationWidthAndHeight()
 
     def initializeGameDisplay(self):
         if self.config.fullscreen:
             self.gameDisplay = pygame.display.set_mode((self.config.displayWidth, self.config.displayHeight), pygame.FULLSCREEN)
         else:
             self.gameDisplay = pygame.display.set_mode((self.config.displayWidth, self.config.displayHeight), pygame.RESIZABLE)
+
+    def initializeLocationWidthAndHeight(self):
+        x, y = self.gameDisplay.get_size()
+        self.locationWidth = x/self.environment.getGrid().getRows()
+        self.locationHeight = y/self.environment.getGrid().getColumns()
+
+    # Draws the environment in its entirety.
+    def drawEnvironment(self):
+        for location in self.environment.getGrid().getLocations():
+            self.drawLocation(location, location.getX() * self.locationWidth, location.getY() * self.locationHeight, self.locationWidth, self.locationHeight)
+
+    # Returns the color that a location should be displayed as.
+    def getColorOfLocation(self, location):
+        if location == -1:
+            color = self.config.white
+        else:
+            color = self.config.white
+            if location.getNumEntities() > 0:
+                color = self.config.black
+        return color
+
+    # Draws a location at a specified position.
+    def drawLocation(self, location, xPos, yPos, width, height):
+        color = self.getColorOfLocation(location)
+        self.graphik.drawRectangle(xPos, yPos, width, height, color)
 
     def quitApplication(self):
         pygame.quit()
@@ -40,12 +69,17 @@ class Serpens:
             self.initializeGameDisplay()
     
     def run(self):
+        self.environment.addEntity(SnakePart())
+        self.environment.printInfo()
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.quitApplication()
                 elif event.type == pygame.KEYDOWN:
                     self.handleKeyDownEvent(event.key)
+
+            self.drawEnvironment()
+            pygame.display.update()
 
 serpens = Serpens()
 serpens.run()
---
+++ src/snakePart.py
@@ -0,0 +1,8 @@
+from entity import Entity
+
+
+# @author Daniel McCoy Stephenson
+# @since August 6th, 2022
+class SnakePart(Entity):
+    def __init__(self):
+        Entity.__init__(self, "Snake Part")
