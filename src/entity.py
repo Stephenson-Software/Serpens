66d8fc6505b8b6f2250f4d3c6be51af0099b11df
dmccoystephenson <dmccoystephenson@gmail.com>
2022-08-08 17:58:46 -0600

    Updated py_env_lib files.

--- src/entity.py
+++ src/entity.py
@@ -6,8 +6,9 @@ import uuid
 
 # @author Daniel McCoy Stephenson
 # @since July 1st, 2022
+#
+# Represents an entity that can exist in a location.
 class Entity(object):
-
     def __init__(self, name):
         self.id = uuid.uuid4()
         self.name = name
@@ -16,45 +17,55 @@ class Entity(object):
         self.gridID = -1
         self.locationID = -1
     
+    # Returns the ID of this entity.
     def getID(self):
         return self.id
     
+    # Returns the name of this entity.
     def getName(self):
         return self.name
 
+    # Returns the ID of the environment that this entity is in.
     def getEnvironmentID(self):
         return self.environmentID
 
+    # Returns the creation timestamp for this entity.
     def getCreationDate(self):
         return self.creationDate
 
-    def getEnvironmentID(self):
-        return self.environmentID
-
+    # Returns the ID of the grid that this entity is in.
     def getGridID(self):
         return self.gridID
 
+    # Returns the ID of the location that this entity is in.
     def getLocationID(self):
         return self.locationID
     
+    # Sets the ID of this entity.
     def setID(self, id):
         self.id = id
 
+    # Sets the name of this entity.
     def setName(self, name):
         self.name = name
 
+    # Sets the environment ID for this entity.
     def setEnvironmentID(self, environmentID):
         self.environmentID = environmentID
     
+    # Sets the creation timestamp for this entity.
     def setCreationDate(self, creationDate):
         self.creationDate = creationDate
 
+    # Sets the grid ID for this entity.
     def setGridID(self, gridID):
         self.gridID = gridID
 
+    # Sets the location ID for this entity.
     def setLocationID(self, locationID):
         self.locationID = locationID
     
+    # Prints information about this entity to the console.
     def printInfo(self):
         print("--------------")
         print(self.name)
